e69f27eebb0fa30288aea8c46c291c8fca5d3217
adityarev <aditya.vritalia@gmail.com>
2019-04-02 14:56:06 +0700

    Initial server

--- tugas3/server/index.js
+++ tugas3/server/index.js
@@ -1,36 +1,16 @@
 import express from 'express'
-import * as User from './api/user'
 
 const app = express()
 const PORT = 5000
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+app.post('/login', (req, res) => {
+    const { username, password } = req.params
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Put validation here
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+    res.status(200).json({ message: "OK" });
 })
 
-app.post('/api/auth/login', User.login)
-app.post('/api/auth/register', User.register)
-app.post('/api/auth/logout', User.logout)
-
-app.get('/api/users', User.getUsers)
-app.get('/api/users/:id', User.getUserById)
-
 app.listen(PORT, () => {
-    console.log(`[express Server] Running on port ${PORT}.`)
+    console.log(`[ Express ] Running on port ${PORT}.`)
 })
