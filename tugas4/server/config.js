220ff474888a7587b5637467d9c325eaaf4142d7
adityarev <aditya.vritalia@gmail.com>
2019-05-13 05:05:26 +0700

    Add Admin View Gates

--- tugas4/server/config.js
+++ tugas4/server/config.js
@@ -5,7 +5,7 @@ export const use = app => {
     app.use(express.urlencoded({ extended: true }))
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
         res.setHeader('Access-Control-Allow-Credentials', true)
         next()
