3d8da98465479ee88f8716837ae2b2a752fca451
adityarev <aditya.vritalia@gmail.com>
2019-05-13 05:31:22 +0700

    Add Admin View Groups

---
+++ tugas4/client/src/styledComponents/adminViewGroups.js
@@ -0,0 +1,41 @@
+const AdminViewGroupsStyledComponent = theme => ({
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  fab: {
+    margin: theme.spacing.unit,
+  },
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {},
+})
+
+export default AdminViewGroupsStyledComponent
