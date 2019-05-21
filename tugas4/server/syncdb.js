00d886eace40bb681b5cd2d5a02ecf12da918cad
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2019-05-21 10:21:54 +0700

    feat(server): Add main scripts

---
+++ tugas4/server/syncdb.js
@@ -0,0 +1,24 @@
+import { sequelize } from './api/db/conn'
+import * as models  from './api/db/models'
+
+
+sequelize.sync({ force: true })
+         .then(_ =>
+             models.Group.create({ name: 'Dosen' }))
+
+         .then(_ =>
+             models.Group.create({ name: 'Mahasiswa' }))
+
+         .then(mhs => {
+             models.Gate.create({ name: 'Plaza Lama 1' })
+                        .then(pl1 =>
+                            models.Rules.create({ groupId: mhs.id, gateId: pl1.id, open: 6, close: 15 }) )
+
+             models.Gate.create({ name: 'Plaza Baru 1' })
+                        .then(pb1 =>
+                            models.Rules.create({ groupId: mhs.id, gateId: pb1.id, open: 15, close: 6 }) )
+
+             models.User.create({ groupId: mhs.id, username: 'ronald', password: 'ronald' }) })
+
+         .then(console.log)
+         .catch(console.error)
